a140107ee181cc25f42a910d8d7b5a42049f0d12
ianpatt <ianpatt@gmail.com>
2022-06-16 22:22:59 -0700

    tick version to 0.6.22

--- f4se_common/f4se_version.h
+++ f4se_common/f4se_version.h
@@ -4,10 +4,10 @@
 // these have to be macros so they can be used in the .rc
 #define F4SE_VERSION_INTEGER		0
 #define F4SE_VERSION_INTEGER_MINOR	6
-#define F4SE_VERSION_INTEGER_BETA	21
-#define F4SE_VERSION_VERSTRING		"0, 0, 6, 21"
-#define F4SE_VERSION_PADDEDSTRING	"0023"
-#define F4SE_VERSION_RELEASEIDX		23
+#define F4SE_VERSION_INTEGER_BETA	22
+#define F4SE_VERSION_VERSTRING		"0, 0, 6, 22"
+#define F4SE_VERSION_PADDEDSTRING	"0024"
+#define F4SE_VERSION_RELEASEIDX		24
 
 #define MAKE_EXE_VERSION_EX(major, minor, build, sub)	((((major) & 0xFF) << 24) | (((minor) & 0xFF) << 16) | (((build) & 0xFFF) << 4) | ((sub) & 0xF))
 #define MAKE_EXE_VERSION(major, minor, build)			MAKE_EXE_VERSION_EX(major, minor, build, 0)
@@ -73,6 +73,6 @@
 // information about the state of the game at the time of release
 #define F4SE_TARGETING_BETA_VERSION	0
 #define CURRENT_RELEASE_RUNTIME		RUNTIME_VERSION_1_10_163
-#define CURRENT_RELEASE_F4SE_STR	"0.6.21"
+#define CURRENT_RELEASE_F4SE_STR	"0.6.22"
 
 #endif /* __F4SE_VERSION_H__ */
